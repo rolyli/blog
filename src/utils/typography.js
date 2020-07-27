b0713986354744f319a09ef01b6cd774a1bfb6ff
rj <rj8205562@gmail.com>
2020-07-27 22:53:58 +0100

    Add utils

---
+++ src/utils/typography.js
@@ -0,0 +1,10 @@
+import Typography from "typography"
+const typography = new Typography({
+baseFontSize: "18px",
+baseLineHeight: 1.666,
+headerFontFamily: ["Domine", "sans-serif"],
+bodyFontFamily: ["Open Sans", "sans-serif"],
+})
+
+export default typography
+export const rhythm = typography.rhythm
